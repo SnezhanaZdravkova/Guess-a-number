cffb3981baad0557d3cd0c9ae145d3b5a0b927e9
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2022-08-08 12:28:54 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+used for imports
+"""
+import random
+
+
+def welcome():
+    """
+    Define the decision of the player
+    """
+
+    print("Welcome to 'Guess a number game'! ")
+    print("In this game you will need to guess a number between 1 and 10. ")
+    print("You can guess only one number at a time.")
+
+    name = input("Enter your name, please: \n")
+    playing = input(
+        f"Hello {name}! Do you want to play? "
+        f"Please enter 'Yes' to play or 'Q' to quit: \n")
+    if playing.lower() == "q":
+        quit()
+    else:
+        print("Okay! Let's play :)")
+
+
+welcome()
